55b79024428e123f9fca5fa19082ccd9db67a2ea
sayeed1124 <sayeedhassan1124@gmail.com>
2023-09-21 14:06:04 +0600

    21.09.23
    Splash Screen issue solved

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Future.delayed(const Duration(seconds: 3)).then((value) {
-      Navigator.push(
+      Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => const HomeScreen()));
     });
   }
